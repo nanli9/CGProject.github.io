f97b65312d8803463244f641e6531faeb9bba867
nanli9 <nanli9@asu.edu>
2023-08-13 14:03:54 +0800

    add light and test

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,10 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { STLLoader } from 'three/addons/loaders/STLLoader.js';
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
+
+var mesh;
 
 const container = document.getElementById("container");
 const innerWidth = container.clientWidth;
@@ -14,16 +18,35 @@ container.appendChild( renderer.domElement );
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 1000 );
 const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set( 100, -180, 90 );
+controls.update();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 
-const loader = new STLLoader()
-loader.load(
+//load background image
+const textureloader = new THREE.TextureLoader();
+textureloader.load('assets/R.jfif' , function(texture){
+    scene.background = texture;  
+});
+//add light
+const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(0, 10, 0);
+light.target.position.set(-5, 0, 0);
+scene.add(light);
+scene.add(light.target);
+//load texture
+const texture = textureloader.load( 'https://images.pexels.com/photos/1205301/pexels-photo-1205301.jpeg' );
+const material = new THREE.MeshBasicMaterial( { map: texture, } );
+//load the mesh
+const STLloader = new STLLoader()
+STLloader.load(
     'lantern.stl',
     function (geometry) {
-        const mesh = new THREE.Mesh(geometry, material)
-        scene.add(mesh)
+        mesh = new THREE.Mesh(geometry,material)
+        //scene.add(mesh)
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -32,14 +55,21 @@ loader.load(
         console.log(error)
     }
 )
+const mtlLoader = new MTLLoader();
+const objLoader = new OBJLoader();
+mtlLoader.load('assets/Lantern_OBJ.mtl', (mtl) => {
+  mtl.preload();
+  objLoader.setMaterials(mtl);
+  objLoader.load('assets/Lantern_OBJ.obj', (root) => {
+    scene.add(root);
+  });
+});
 
-camera.position.set( 100, -180, 90 );
-controls.update();
 
+//animate
 function animate() {
 	requestAnimationFrame( animate );
 
-	
 	controls.update();
 	renderer.render( scene, camera );
 }

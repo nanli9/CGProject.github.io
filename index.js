297143900c6226829c704aed427e7cfdecf94587
nanli9 <nanli9@asu.edu>
2023-08-13 17:58:35 +0800

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { STLLoader } from 'three/addons/loaders/STLLoader.js';
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
+
+var mesh;
+const planeSize = 400;
+const textureloader = new THREE.TextureLoader();
 
 const container = document.getElementById("container");
 const innerWidth = container.clientWidth;
@@ -14,15 +20,68 @@ container.appendChild( renderer.domElement );
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 1000 );
 const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set( 100, 100, 90 );
+controls.update();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 
-const loader = new STLLoader()
-loader.load(
+//load background image
+// textureloader.load('assets/R.jfif' , function(texture){
+//     scene.background = texture;  
+// });
+scene.background = new THREE.CubeTextureLoader()
+	.setPath( 'assets/skybox/' )
+	.load( [
+        "right.jpg",
+        "left.jpg",
+        "top.jpg",
+        "bottom.jpg",
+        "front.jpg",
+        "back.jpg"
+			] );
+//load plane
+loadPlane();
+function loadPlane(){
+    const PlaneTexture = textureloader.load('assets/checker.png');
+    PlaneTexture.wrapS = THREE.RepeatWrapping;
+    PlaneTexture.wrapT = THREE.RepeatWrapping;
+    PlaneTexture.magFilter = THREE.NearestFilter;
+    PlaneTexture.colorSpace = THREE.SRGBColorSpace;
+    const repeats = planeSize / 2;
+    PlaneTexture.repeat.set(repeats, repeats);
+    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshPhongMaterial({
+    map: PlaneTexture,
+    side: THREE.DoubleSide,
+    });
+    const mesh = new THREE.Mesh(planeGeo, planeMat);
+    mesh.rotation.x = Math.PI * -.5;
+    scene.add(mesh);
+}
+
+
+
+//add light
+const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(0, 10, 0);
+light.target.position.set(10, 0, 0);
+scene.add(light);
+scene.add(light.target);
+//load texture
+// const texture = textureloader.load( 'https://images.pexels.com/photos/1205301/pexels-photo-1205301.jpeg' );
+const material = new THREE.MeshBasicMaterial( { color:0xFFFFFF} );
+//load the mesh
+const STLloader = new STLLoader()
+STLloader.load(
     'lantern.stl',
     function (geometry) {
-        const mesh = new THREE.Mesh(geometry, material)
+        mesh = new THREE.Mesh(geometry,material);
+        mesh.rotation.x = -Math.PI / 2;
+        mesh.position.set( 50, 50, 50 );
+        mesh.scale.set(0.5,0.5,0.5);
         scene.add(mesh)
     },
     (xhr) => {
@@ -32,14 +91,21 @@ loader.load(
         console.log(error)
     }
 )
+const mtlLoader = new MTLLoader();
+const objLoader = new OBJLoader();
+mtlLoader.load('assets/Lantern_OBJ.mtl', (mtl) => {
+  mtl.preload();
+  objLoader.setMaterials(mtl);
+  //Lantern_low_Lantern_BaseColor.png
+  objLoader.load('assets/Lantern_OBJ.obj', (root) => {
+    scene.add(root);
+  });
+});
 
-camera.position.set( 100, -180, 90 );
-controls.update();
 
+//animate
 function animate() {
 	requestAnimationFrame( animate );
-
-	
 	controls.update();
 	renderer.render( scene, camera );
 }

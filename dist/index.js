e0879f6d0c241bc5018a009f0d4d8d7c20ca90b5
nanli9 <nanli9@asu.edu>
2023-08-15 21:42:26 +0800

    add color material

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,16 @@ import { STLLoader } from 'three/addons/loaders/STLLoader.js';
 import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
 import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
 
-var lantern;
+var bead,maincube,tassels,woodframe,topcube;
+let i =2;
+let lightMoveSpeed=0.0;
+let lanternrotateSpeed=0.0;
+let rotateCheck=false;
+let lightMove = false;
+
 const planeSize = 20;
+
+const STLloader = new STLLoader();
 const textureloader = new THREE.TextureLoader();
 
 const container = document.getElementById("container");
@@ -13,7 +21,10 @@ const innerWidth = container.clientWidth;
 const innerHeight = container.clientHeight;
 console.log(innerWidth,innerHeight);
 
-const renderer = new THREE.WebGLRenderer();
+//antialias
+const renderer = new THREE.WebGLRenderer({antialiasing:true});
+renderer.setPixelRatio( window.devicePixelRatio * 1.5 );
+
 renderer.setSize( innerWidth, innerHeight );
 renderer.shadowMap.enabled = true;
 container.appendChild( renderer.domElement );
@@ -31,6 +42,7 @@ controls.update();
 // textureloader.load('assets/R.jfif' , function(texture){
 //     scene.background = texture;  
 // });
+
 scene.background = new THREE.CubeTextureLoader()
 	.setPath( 'assets/skybox/' )
 	.load( [
@@ -66,13 +78,18 @@ function loadPlane(){
 const color = 0xFFFFFF;
 const intensity = 1;
 const light = new THREE.DirectionalLight(color, intensity);
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
 light.castShadow = true;
-light.position.set(3, 5, 0);
+light.position.set(i, 5, 0);
 light.target.position.set(0, 0, 0);
 scene.add(light);
 scene.add(light.target);
+scene.add(ambientLight);
+
 const lightPoint = new THREE.Mesh( new THREE.BoxGeometry( 0.1, 0.1, 0.1 ), new THREE.MeshBasicMaterial( { color: 0xffff2f } ) );
+
 markLight();
+
 function markLight(){
     scene.add( lightPoint );
 }
@@ -82,35 +99,49 @@ function updateLight(i){
 addInnerLightSource();
 function addInnerLightSource(){
     const color = 0xFFFFFF;
-    const intensity = 0.6;
+    const intensity = 0.4;
     const light = new THREE.PointLight(color, intensity);
     light.position.set(0, 3, 1);
     scene.add(light);
 }
 
-const STLloader = new STLLoader()
 loadLantern();
 //load the mesh
 function loadLantern(){
-    //load texture
-    const texture = textureloader.load( 'https://images.pexels.com/photos/1205301/pexels-photo-1205301.jpeg' );
-    const material = new THREE.MeshBasicMaterial( { color:0x000000, ambient:0x99CC3B,} );
+    //mark inner light source
+    const cube = new THREE.Mesh( new THREE.BoxGeometry( 0.1, 0.1, 0.1 ), new THREE.MeshBasicMaterial( { color: 0xffff2f } ) );
+    cube.position.set(0, 2.5, 1);
+    scene.add( cube );
     STLloader.load(
-        'lantern.stl',
+        'assets/lantern/bead.stl',
         function (geometry) {
-            lantern = new THREE.Mesh(geometry,material);
-            lantern.rotation.x = -Math.PI / 2;
-    
-            //mark inner light source
-            const cube = new THREE.Mesh( new THREE.BoxGeometry( 0.1, 0.1, 0.1 ), new THREE.MeshBasicMaterial( { color: 0xffff2f } ) );
-            cube.position.set(0, 2.5, 1);
-            scene.add( cube );
-    
+            const material = new THREE.MeshPhongMaterial( { color:0x76d2aa} );
+            bead = new THREE.Mesh(geometry,material);
+            bead.rotation.x = -Math.PI / 2;
             //mesh.position.set( 3, 2.5, 1 );
-            lantern.position.set( 0, 2.5, 1 );
-            lantern.scale.set(0.02,0.02,0.02);
-            lantern.castShadow = true;
-            scene.add(lantern);
+            bead.position.set( 0, 2.5, 1 );
+            bead.scale.set(0.02,0.02,0.02);
+            bead.castShadow = true;
+            scene.add(bead);
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+        },
+        (error) => {
+            console.log(error)
+        }
+    )
+    STLloader.load(
+        'assets/lantern/maincube.stl',
+        function (geometry) {
+            const material = new THREE.MeshPhongMaterial( { color:0xefc090} );
+            maincube = new THREE.Mesh(geometry,material);
+            maincube.rotation.x = -Math.PI / 2;
+            //mesh.position.set( 3, 2.5, 1 );
+            maincube.position.set( 0, 2.5, 1 );
+            maincube.scale.set(0.02,0.02,0.02);
+            maincube.castShadow = true;
+            scene.add(maincube);
         },
         (xhr) => {
             console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -119,6 +150,65 @@ function loadLantern(){
             console.log(error)
         }
     )
+    STLloader.load(
+        'assets/lantern/tassels.stl',
+        function (geometry) {
+            const material = new THREE.MeshPhongMaterial( { color:0xd97070} );
+            tassels = new THREE.Mesh(geometry,material);
+            tassels.rotation.x = -Math.PI / 2;
+            //mesh.position.set( 3, 2.5, 1 );
+            tassels.position.set( 0, 2.5, 1 );
+            tassels.scale.set(0.02,0.02,0.02);
+            tassels.castShadow = true;
+            scene.add(tassels);
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log(error)
+        }
+    )
+    STLloader.load(
+        'assets/lantern/woodframe.stl',
+        function (geometry) {
+            const material = new THREE.MeshPhongMaterial( { color:0x4c4040} );
+            woodframe = new THREE.Mesh(geometry,material);
+            woodframe.rotation.x = -Math.PI / 2;
+            //mesh.position.set( 3, 2.5, 1 );
+            woodframe.position.set( 0, 2.5, 1 );
+            woodframe.scale.set(0.02,0.02,0.02);
+            woodframe.castShadow = true;
+            scene.add(woodframe);
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log(error)
+        }
+    )
+    STLloader.load(
+        'assets/lantern/topcube.stl',
+        function (geometry) {
+            const material = new THREE.MeshPhongMaterial( { color:0xefc090} );
+            topcube = new THREE.Mesh(geometry,material);
+            topcube.rotation.x = -Math.PI / 2;
+            //mesh.position.set( 3, 2.5, 1 );
+            topcube.position.set( 0, 2.5, 1 );
+            topcube.scale.set(0.02,0.02,0.02);
+            topcube.castShadow = true;
+            scene.add(topcube);
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log(error)
+        }
+    )
+    // //load texture
+    // const texture = textureloader.load( 'https://images.pexels.com/photos/1205301/pexels-photo-1205301.jpeg' );
 }
 
 const mtlLoader = new MTLLoader();
@@ -137,21 +227,23 @@ mtlLoader.load('assets/Lantern_OBJ.mtl', (mtl) => {
   });
 });
 
-let i =0;
-let lightMoveSpeed=0.0;
-let lanternrotateSpeed=0.0;
+
 rotateButton();
 lightMoveButton();
-let rotateCheck=false;
-let lightMove = false;
+
 //animate
 function animate() {
 	requestAnimationFrame( animate );
     i+=lightMoveSpeed;
     light.position.set(i, 5, 0);
     updateLight(i);
-    if(lantern!=undefined)
-        lantern.rotation.z+=lanternrotateSpeed;
+    if(bead&&maincube&&tassels&&topcube&&woodframe){
+        bead.rotation.z+=lanternrotateSpeed;
+        maincube.rotation.z+=lanternrotateSpeed;
+        tassels.rotation.z+=lanternrotateSpeed;
+        topcube.rotation.z+=lanternrotateSpeed;
+        woodframe.rotation.z+=lanternrotateSpeed;
+    }
 	controls.update();
 	renderer.render( scene, camera );
 }
